82eaa1d97b1001268fcc737c7c059d19649c4c83
thanghuy <huythang0903@gmail.com>
2020-11-01 12:08:33 +0700

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace tour.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
