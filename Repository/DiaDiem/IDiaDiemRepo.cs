4c0e26a0d610ca2768f1860a47d4c43fa07d4939
Ninh Ngoc Hieu <30309069+ninhngochieu@users.noreply.github.com>
2020-11-08 13:16:11 +0700

    commit

---
+++ Repository/DiaDiem/IDiaDiemRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tour.Models;
+
+namespace tour.Repository.DiaDiem
+{
+    public interface IDiaDiemRepo
+    {
+        DiaDiems Get(int id);
+        bool Add(DiaDiems d);
+        bool Update(DiaDiems d);
+        List<DiaDiems> GetAllDiaDiems();
+        int Count();
+    }
+}
